e5deb86434b7130e9a37a9e40184a9851694d8d3
Yaseen Lotfi <yalotfi@outlook.com>
2016-12-01 16:51:54 -0500

    Update election2016.R

--- election2016.R
+++ election2016.R
@@ -1,14 +1,8 @@
+#####################
+### Load Packages ###
+#####################
+## Call script
 source("loadPackages.R")
-
-##########################
-### Set-Up Twitter API ###
-##########################
-api_key = "INSERT HERE"
-api_secret = "INSERT HERE"
-token_key = "INSERT HERE"
-token_secret = "INSERT HERE"
-setup_twitter_oauth(api_key, api_secret, token_key, token_secret)
-rm(api_key, api_secret, token_key, token_secret)
 
 
 ###########################
@@ -26,11 +20,51 @@ states <- readOGR(
 center_pts <- gCentroid(states, byid = T)
 states@data$lon <- center_pts@coords[, 1]
 states@data$lat <- center_pts@coords[, 2]
-rm(center_pts)
+
+## Define UTM Zone for each State
+utm <- matrix(
+  data = c(seq(-66, -126, -6),  # degree range
+           seq(19, 9, -1)  # UTM Zones
+           )
+  , nrow = 11, ncol = 2  # 10 zones = 10 rows, 2 variables
+  , byrow = FALSE  # Fill matrix column-wise
+  , dimnames = list(NULL, c("lon_range", "zones"))  # Null row names, define col names
+)
+utm <- as.data.frame(utm)  # coerce matrix to df
+
+## Assign UTM Zone for each State
+states@data$zone <- NA
+for (i in 0:nrow(states@data)-1) {
+  if (states@data$lon_center[i] > min(utm[ ,1]) && states@data$lon_center[i] < max(utm[ ,1])) {
+    for (j in 1:nrow(utm)) {
+      if (states@data$lon_center[i] < utm$lon_range[j] && states@data$lon_center[i] > utm$lon_range[j+1]) {
+        
+      } else {
+        next
+      }
+    }
+  } else {
+    next
+  }
+}
 
 ## Filter States (Minus non-Contintental US, Plus DC)
+states_f <- fortify(states)
+states@data$id <- as.character(0:(nrow(states@data)-1))
+states.data <- left_join(states_f, states@data, by = c("id"= "id"))
 states.data <- states@data %>% filter(STUSPS %in% 
                                       c(state.abb[-which(state.abb == "AK" | state.abb == "HI")], "DC"))  # state.abb is a R dataset
+
+
+##########################
+### Set-Up Twitter API ###
+##########################
+api_key = "INSERT HERE"
+api_secret = "INSERT HERE"
+token_key = "INSERT HERE"
+token_secret = "INSERT HERE"
+setup_twitter_oauth(api_key, api_secret, token_key, token_secret)
+rm(api_key, api_secret, token_key, token_secret)
 
 
 ########################
